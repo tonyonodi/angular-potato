54ff68b5e1a675ae903d82f5f61e0748fdeb7f67
Tony Onodi <tonyonodi@gmail.com>
2014-10-09 13:35:16 +0100

    Initial commit.

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name potatoApp
+ * @description
+ * # potatoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('potatoApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
