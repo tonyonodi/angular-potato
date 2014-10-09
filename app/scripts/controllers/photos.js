2e53500e981e7659b259cd228eb2b8f83858bcef
Tony Onodi <tonyonodi@gmail.com>
2014-10-09 13:49:56 +0100

    Created photos controller.

---
+++ app/scripts/controllers/photos.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name potatoApp.controller:PhotosCtrl
+ * @description
+ * # PhotosCtrl
+ * Controller of the potatoApp
+ */
+angular.module('potatoApp')
+  .controller('PhotosCtrl', function ($scope) {
+    $scope.feedItems = [
+
+    ];
+  });
