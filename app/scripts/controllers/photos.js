aad45e28252f0b5ea40b09102a2057c5e50eaf21
Tony Onodi <tonyonodi@gmail.com>
2014-10-12 02:48:17 +0100

    Add ID property to item object.

--- app/scripts/controllers/photos.js
+++ app/scripts/controllers/photos.js
@@ -9,8 +9,10 @@
  */
 
 var getUrl = "http://localhost:8888/http://api.flickr.com/services/feeds/photos_public.gne?tags=potato&tagmode=all&format=json&api_key=19125c0693fd4a10163b5fb92c2c37e8",
-    cleanseResponse;
+    cleanseResponse,
+    processJSON;
 
+// removes extraneous text from retrieved JSON string
 cleanseResponse = function(response) {
     if ( response.indexOf("jsonFlickrFeed(") == -1 ) {
         return "unable to cleanse data"
@@ -19,15 +21,35 @@ cleanseResponse = function(response) {
     return response.slice(15, -1)
 }
 
+// creates photo ID, processes date, truncates title text
+processJSON = function( list ) {
+    var i,
+        link,
+        date;
+
+    for (i = 0; i < list.length; i++) {
+        link = list[i].link;
+        date = list[i].date;
+
+        link = link.split("/");
+        link = link[link.length - 2];
+        list[i].id = link;
+    }
+
+    return list;
+}
+
 angular.module('potatoApp')
   .controller('PhotosCtrl', function ($scope, $http) {
 
     $http.get(getUrl).success(function(data, status, headers, config) {
-        var cleansedData;
+        var cleansedData,
+            processedJSON;
 
-        cleansedData = cleanseResponse(data);
+        processedJSON = JSON.parse(cleanseResponse(data)).items;
+        processedJSON = processJSON(processedJSON);
 
-        $scope.feedItems = JSON.parse(cleansedData).items;
+        $scope.feedItems = processedJSON;
     }).error(function(data, status, headers, config) {
         console.log("ERROR");
     });
