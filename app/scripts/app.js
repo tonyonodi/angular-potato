952b2e2c4751b35a47e4a63f71206a2d8d74e1a7
Tony Onodi <tonyonodi@gmail.com>
2014-10-12 12:41:50 +0100

    Specify controller with routing rather than in view.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -21,7 +21,7 @@ angular
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
+        controller: 'PhotosCtrl'
       })
       .when('/about', {
         templateUrl: 'views/about.html',
