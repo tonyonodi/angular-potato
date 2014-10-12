357c2a3f30bafb39b116bbba635fa6d2f743f027
Tony Onodi <tonyonodi@gmail.com>
2014-10-12 14:18:22 +0100

    Finding photos in controller for photo detail page.

--- app/scripts/controllers/photodetail.js
+++ app/scripts/controllers/photodetail.js
@@ -7,7 +7,29 @@
  * # PhotodetailCtrl
  * Controller of the potatoApp
  */
+
 angular.module('potatoApp')
-  .controller('PhotoDetailCtrl', function ($scope, $routeParams) {
-    $scope.photoId = $routeParams.photoId;
+  .controller('PhotoDetailCtrl', function ($scope, $http, $routeParams) {
+  	var idMatches;
+
+  	idMatches = function(element) {
+	  if ( element.id == $routeParams.photoId ) {
+		return element;
+	  }
+	}
+
+    $http.get(getUrl).success(function(data, status, headers, config) {
+        var cleansedData,
+            processedJSON,
+            filteredList;
+
+        processedJSON = JSON.parse(cleanseResponse(data)).items;
+        processedJSON = processJSON(processedJSON);
+
+		filteredList = processedJSON.filter(idMatches);
+
+        $scope.photo = filteredList[0];
+    }).error(function(data, status, headers, config) {
+        console.log("ERROR");
+    });
   });
