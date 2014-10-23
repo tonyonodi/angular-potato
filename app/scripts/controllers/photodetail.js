469c719071e99e5cbc335e8d1035a1a3ddafbfa5
Tony Onodi <tonyonodi@gmail.com>
2014-10-23 02:12:13 +0100

    Rebuilt with rootURL bugfixes.

---
+++ app/scripts/controllers/photodetail.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name potatoApp.controller:PhotodetailCtrl
+ * @description
+ * # PhotodetailCtrl
+ * Controller of the potatoApp
+ */
+
+angular.module('potatoApp')
+  .controller('PhotoDetailCtrl', function ($scope, $http, $routeParams) {
+  	var idMatches;
+
+  	idMatches = function(element) {
+	  if ( element.id == $routeParams.photoId ) {
+		return element;
+	  }
+	}
+
+    $http.get(getUrl).success(function(data, status, headers, config) {
+        var cleansedData,
+            processedJSON,
+            filteredList;
+        $scope.rootURL = "/angular-potato";    
+
+        processedJSON = processJSON(data.items);
+
+    		filteredList = processedJSON.filter(idMatches);
+
+        $scope.photo = filteredList[0];
+    }).error(function(data, status, headers, config) {
+        console.log("ERROR");
+    });
+  });
---
+++ app/scripts/controllers/photos.js
@@ -0,0 +1,76 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name potatoApp.controller:PhotosCtrl
+ * @description
+ * # PhotosCtrl
+ * Controller of the potatoApp
+ */
+
+var getUrl = "https://floating-brook-4021.herokuapp.com",
+    cleanseResponse,
+    processJSON;
+
+// creates photo ID, processes date, truncates title text
+processJSON = function( list ) {
+    var i,
+        link,
+        dateString,
+        tagString,
+        dateObj,
+        date, month, year, hours, minutes,
+        prettyDateString,
+        dateSuffix = { 1: "st", 2: "nd", 3: "rd", 21: "st", 22: "nd", 23: "rd", 31: "st"},
+        monthList = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+        tagArray;
+
+    for (i = 0; i < list.length; i++) {
+        // retrieve from photo object
+        link = list[i].link;
+        dateString = list[i].date_taken;
+        tagString = list[i].tags;
+
+        // get unique photo identifier
+        link = link.split("/");
+        link = link[link.length - 2];
+        list[i].id = link;
+
+        // make pretty date
+        dateObj = new Date(dateString);
+        
+        date = dateObj.getDate();
+        date =  date + (dateSuffix[date] || "th");
+        month = monthList[ dateObj.getMonth() ];
+        year = dateObj.getFullYear();
+        hours = dateObj.getHours();
+        minutes = dateObj.getMinutes();
+
+        prettyDateString = date + " " + month + " " + year + " at " + hours + ":" + minutes;
+
+        list[i].pretty_date_taken = prettyDateString;
+
+        tagArray = tagString.split(" ");
+
+        list[i].tags = tagArray;
+
+    }
+
+    return list;
+}
+
+angular.module('potatoApp')
+  .controller('PhotosCtrl', function ($scope, $http) {
+
+    $http.get(getUrl).success(function(data, status, headers, config) {
+        var processedJSON;
+        $scope.rootURL = "/angular-potato";    
+
+        processedJSON = processJSON(data.items);
+
+        $scope.feedItems = processedJSON;
+    }).error(function(data, status, headers, config) {
+        console.log("ERROR");
+    });
+
+  });
